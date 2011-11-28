701bd3f98dfb975c48bf1a6a5a14c4a1f7a4347b
Pete Zaitcev <zaitcev@yahoo.com>
2011-11-27 21:30:14 -0700

    Implement a proper skb_pull for the future.
    We never send two messages in a row currently, there's always a strict
    ping-pong. This flushes the socket and thus skb_pull is never used
    to extract the current message, leaving the next one in the pipe.
    However, we do not want to forget about the skb_buff being fake and
    be frustrated in the future.

--- forumlib/__init__.py
+++ forumlib/__init__.py
@@ -5,7 +5,8 @@
 # See file COPYING for licensing information (expect GPL 2).
 #
 
-# Pull a bytearray of size bytes out of the list of strings, copy it out.
+# Copy out a bytearray of size bytes out of the list of strings.
+# For now, do not requiest the size that is larger than the size available.
 def skb_pull_copy(mbufs, size):
     v = bytearray(size)
     mx = 0
@@ -24,9 +25,23 @@ def skb_pull_copy(mbufs, size):
            mx += 1
     return v
 
-# Pull a bytearray from mbufs and (in the future XXX) remove from mbufs,
-# in order to permit repeated pull calls when the message length is known.
-# XXX For now, we just do nothing... so it's the same deal as skb_pull_copy.
+# Pull a bytearray from mbufs, in order to permit repeated pull calls.
 def skb_pull(mbufs, size):
-    return skb_pull_copy(mbufs, size)
-
+    v = bytearray(size)
+    x = 0
+    done = 0
+    while done < size:
+       mbuf = mbufs[0]
+       steplen = size - done
+       if len(mbuf) < steplen:
+           steplen = len(mbuf)
+       v[done:done+steplen] = mbuf[x:x+steplen]
+       done += steplen
+       x += steplen
+       if x >= len(mbuf):
+           x = 0
+           mbufs.pop(0)
+    if x != 0:
+       mbuf = mbufs[0]	# unnecessary but feels safer
+       mbufs[0] = mbuf[x:]
+    return v
